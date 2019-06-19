b04380b343744eae82cf2e1491e6678bb4bce3f3
Nav <navdeepsinghgirn@gmail.com>
2019-06-19 20:44:16 +0100

    Added in a chart

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ function calculate() {
 
   // The equation is A = p * [[1 + (r/n)] ^ nt]
   A = p * Math.pow(1 + r / (n * 100), n * t);
+  VGSLXResult = A.toFixed(2);
+
 
   // toFixed is used for rounding the amount with two decimal places.
   result.innerHTML =
@@ -26,6 +28,7 @@ function calculate() {
 
   // The equation is A = p * [[1 + (r/n)] ^ nt]
   A = p * Math.pow(1 + r / (n * 100), n * t);
+  VFIAXResult = A.toFixed(2);
 
   // toFixed is used for rounding the amount with two decimal places.
   result.innerHTML =
@@ -45,6 +48,7 @@ function calculate() {
 
   // The equation is A = p * [[1 + (r/n)] ^ nt]
   A = p * Math.pow(1 + r / (n * 100), n * t);
+  VBTLXResult = A.toFixed(2);
 
   // toFixed is used for rounding the amount with two decimal places.
   result.innerHTML =
@@ -53,4 +57,67 @@ function calculate() {
   result.innerHTML +=
     "<br>Total profit over the investment period £" +
     (A.toFixed(2) - p).toFixed(2);
+
+  let myChart = document.getElementById("myChart").getContext("2d");
+
+  // Global Options
+  Chart.defaults.global.defaultFontFamily = 'Lato';
+  Chart.defaults.global.defaultFontSize = 18;
+  Chart.defaults.global.defaultFontColor = '#777';
+
+  let massPopChart = new Chart(myChart, {
+    type:'bar', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+    data:{
+      labels:['VGSLX', 'VFIAX', 'VBTLX'],
+      datasets:[{
+        label:'Fund Returns',
+        data:[
+          VGSLXResult,
+          VFIAXResult,
+          VBTLXResult
+        ],
+        //backgroundColor:'green',
+        backgroundColor:[
+          'rgba(255, 99, 132, 0.6)',
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(255, 206, 86, 0.6)',
+          'rgba(75, 192, 192, 0.6)',
+          'rgba(153, 102, 255, 0.6)',
+          'rgba(255, 159, 64, 0.6)',
+          'rgba(255, 99, 132, 0.6)'
+        ],
+        borderWidth:1,
+        borderColor:'#777',
+        hoverBorderWidth:3,
+        hoverBorderColor:'#000'
+      }]
+    },
+    options:{
+      title:{
+        display:true,
+        text:'Index Fund Returns',
+        fontSize:25
+      },
+      legend:{
+        display:true,
+        position:'right',
+        labels:{
+          fontColor:'#000'
+        }
+      },
+      layout:{
+        padding:{
+          left:50,
+          right:0,
+          bottom:0,
+          top:0
+        }
+      },
+      tooltips:{
+        enabled:true
+      }
+    }
+  });
+
+
 }
