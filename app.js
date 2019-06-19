8eabe234a323928c1c9356e58239d843d2040022
Nav <navdeepsinghgirn@gmail.com>
2019-06-19 20:09:17 +0100

    Everything works. Basic css applied

--- app.js
+++ app.js
@@ -10,9 +10,12 @@ function calculate() {
   A = p * Math.pow(1 + r / (n * 100), n * t);
 
   // toFixed is used for rounding the amount with two decimal places.
-  result.innerHTML = "Total amount you will have after the investment period £" + A.toFixed(2);
+  result.innerHTML =
+    "Total amount you will have after the investment period £" + A.toFixed(2);
 
-  result.innerHTML += "<br>Total profit over the investment period £" + (A.toFixed(2) - p).toFixed(2);
+  result.innerHTML +=
+    "<br>Total profit over the investment period £" +
+    (A.toFixed(2) - p).toFixed(2);
 
   //VFIAX Calculation
   p = document.getElementById("p").value;
@@ -25,9 +28,13 @@ function calculate() {
   A = p * Math.pow(1 + r / (n * 100), n * t);
 
   // toFixed is used for rounding the amount with two decimal places.
-  result.innerHTML = "Total amount you will have after the investment period £" + A.toFixed(2);
+  result.innerHTML =
+    "Total estimated amount you will have after the investment period £" +
+    A.toFixed(2);
 
-  result.innerHTML += "<br>Total profit over the investment period £" + (A.toFixed(2) - p).toFixed(2);
+  result.innerHTML +=
+    "<br>Total estimated profit over the investment period £" +
+    (A.toFixed(2) - p).toFixed(2);
 
   //VBTLX Calculation
   p = document.getElementById("p").value;
@@ -40,9 +47,10 @@ function calculate() {
   A = p * Math.pow(1 + r / (n * 100), n * t);
 
   // toFixed is used for rounding the amount with two decimal places.
-  result.innerHTML = "Total amount you will have after the investment period £" + A.toFixed(2);
+  result.innerHTML =
+    "Total amount you will have after the investment period £" + A.toFixed(2);
 
-  result.innerHTML += "<br>Total profit over the investment period £" + (A.toFixed(2) - p).toFixed(2);
-
-
+  result.innerHTML +=
+    "<br>Total profit over the investment period £" +
+    (A.toFixed(2) - p).toFixed(2);
 }
