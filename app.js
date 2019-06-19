a101beddbf0c6a212ebd4b8bb2bdd08f1442af26
Nav <navdeepsinghgirn@gmail.com>
2019-06-19 19:39:41 +0100

    Allows for calculations to be done for each fund based on a single input for comparisons

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
 function calculate() {
-  //p = document.getElementById("p").value;
-  //n = document.getElementById("n").value; // no. of compoundings per year
-  //t = document.getElementById("t").value; // no. of years
-  //r = document.getElementById("r").value;
-  result = document.getElementById("result");
+  //VGSLX Calculation
+  p = document.getElementById("p").value;
+  n = "1"; // no. of compoundings per year
+  t = document.getElementById("t").value; // no. of years
+  r = "10.53";
+  result = document.getElementById("result-VGSLX");
 
   // The equation is A = p * [[1 + (r/n)] ^ nt]
-  //A = p * Math.pow(1 + r / (n * 100), n * t);
+  A = p * Math.pow(1 + r / (n * 100), n * t);
 
   // toFixed is used for rounding the amount with two decimal places.
-  result.innerHTML = "TEST";
+  result.innerHTML = "Total amount you will have after the investment period £" + A.toFixed(2);
 
-  //result.innerHTML = "The total amount is " + A.toFixed(2);
+  result.innerHTML += "<br>Total profit over the investment period £" + (A.toFixed(2) - p).toFixed(2);
 
-  //result.innerHTML += "<br> The interest is " + (A.toFixed(2) - p).toFixed(2);
+  //VFIAX Calculation
+  p = document.getElementById("p").value;
+  n = "1"; // no. of compoundings per year
+  t = document.getElementById("t").value; // no. of years
+  r = "6.17";
+  result = document.getElementById("result-VFIAX");
+
+  // The equation is A = p * [[1 + (r/n)] ^ nt]
+  A = p * Math.pow(1 + r / (n * 100), n * t);
+
+  // toFixed is used for rounding the amount with two decimal places.
+  result.innerHTML = "Total amount you will have after the investment period £" + A.toFixed(2);
+
+  result.innerHTML += "<br>Total profit over the investment period £" + (A.toFixed(2) - p).toFixed(2);
+
+  //VBTLX Calculation
+  p = document.getElementById("p").value;
+  n = "1"; // no. of compoundings per year
+  t = document.getElementById("t").value; // no. of years
+  r = "4.07";
+  result = document.getElementById("result-VBTLX");
+
+  // The equation is A = p * [[1 + (r/n)] ^ nt]
+  A = p * Math.pow(1 + r / (n * 100), n * t);
+
+  // toFixed is used for rounding the amount with two decimal places.
+  result.innerHTML = "Total amount you will have after the investment period £" + A.toFixed(2);
+
+  result.innerHTML += "<br>Total profit over the investment period £" + (A.toFixed(2) - p).toFixed(2);
+
+
 }
